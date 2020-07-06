d302af44b6d161bbdf7e91af9538f18b0b4da13b
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-07-06 00:57:10 -0500

    refactor(index.js): change to async/await style

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
 'use strict'
 
 const fp = require('fastify-plugin')
+
 const startTimeSymbol = Symbol('startTime')
 
-const fastifyDatadog = (fastify, {
-  dogstatsd,
-  stat = 'node.fastify.router',
-  tags = [],
-  method = false,
-  path = false,
-  responseCode = false
-} = {}, done) => {
+async function fastifyDatadog (fastify, options = {}) {
+  const {
+    dogstatsd,
+    stat = 'node.fastify.router',
+    tags = [],
+    method = false,
+    path = false,
+    responseCode = false
+  } = options
+
   if (dogstatsd == null) {
     throw new Error('Missing dogstatsd option.')
   }
 
   const now = () => {
     const [seconds, nanoseconds] = process.hrtime()
+
     return seconds * 1e3 + nanoseconds / 1e6
   }
 
-  fastify.addHook('onRequest', (request, reply, next) => {
-    request.raw[startTimeSymbol] = now()
-    next()
+  fastify.addHook('onRequest', async (req, reply) => {
+    req[startTimeSymbol] = now()
   })
 
-  fastify.addHook('onSend', (request, reply, payload, next) => {
-    const { raw: req } = request
+  fastify.addHook('onSend', async (req, reply) => {
+    const { context, statusCode } = reply
 
-    const { context, raw: { statusCode } } = reply
-
-    const { config: { url: route } } = context
-
-    const statTags = [`route:${route}`, ...tags]
+    const statTags = [`route:${context.config.url}`, ...tags]
 
     if (method) {
       statTags.push(`method:${req.method.toLowerCase()}`)
     }
 
-    if (path !== false) {
+    if (path) {
       statTags.push(`path:${req.url}`)
     }
 
@@ -48,12 +47,10 @@ const fastifyDatadog = (fastify, {
       dogstatsd.increment(`${stat}.response_code.all`, 1, statTags)
     }
 
-    dogstatsd.histogram(`${stat}.response_time`, now() - req[startTimeSymbol], 1, statTags)
+    const resposeTime = now() - req[startTimeSymbol]
 
-    next()
+    dogstatsd.histogram(`${stat}.response_time`, resposeTime, 1, statTags)
   })
-
-  done()
 }
 
 module.exports = fp(fastifyDatadog, {
